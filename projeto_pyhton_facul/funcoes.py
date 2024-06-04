3a3b1f17d47d644484f3606ff42432530ec55415
viniciusvilla <120747413+viniciusvilla@users.noreply.github.com>
2024-06-03 21:29:02 -0300

    Projeto Final
    Add banco de dados; excluir; editar; gerar pdf

--- projeto_pyhton_facul/funcoes.py
+++ projeto_pyhton_facul/funcoes.py
@@ -1,17 +1,117 @@
+from PyQt5.QtWidgets import QMessageBox
+import mysql.connector
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from PyQt5.QtWidgets import QFileDialog
 
-def test_vazio():
-    print
+
+def excluir_dados(tela_banco, banco):
+
+    msg = QMessageBox(tela_banco)
+    msg.setStyleSheet("background-color: #f1f1f1;")
+    msg.setWindowTitle('Confirmar Exclusão')
+    msg.setText('Tem certeza que deseja EXCLUIR a linha selecionada?')
+    msg.setIcon(QMessageBox.Question)
+    msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+
+    button_yes = msg.button(QMessageBox.Yes)
+    button_yes.setText('Sim')
+    button_no = msg.button(QMessageBox.No)
+    button_no.setText('Não')
+
+    resposta = msg.exec_()
+
+    if resposta == QMessageBox.No:
+        return
+
+    linha_selecionada = tela_banco.tableWidget.currentRow()
+    tela_banco.tableWidget.removeRow(linha_selecionada)
+    print(f'Linha selecionada {linha_selecionada}')
+
+    cursor = banco.cursor()
+    cursor.execute("SELECT id FROM produtos")
+    dados_lidos = cursor.fetchall()
+    print(f'Dados lidos: {dados_lidos}')
+    valor_id = dados_lidos[linha_selecionada][0]
+    print(f'Dados lidos no indice linha selecionada: {valor_id}')
+
+    cursor.execute("DELETE FROM produtos WHERE id=" + str(valor_id))
+    banco.commit()
 
 
-def limpar_campos(tela):
+def gerar_pdf(tela_cadastro, banco, tela_banco):
+    try:
+        pdf_cursor = banco.cursor()
+        comando_SQL = "SELECT * FROM produtos"
+        pdf_cursor.execute(comando_SQL)
+        dados_lidos = pdf_cursor.fetchall()
 
-    tela.produtoTipo.clear()
-    tela.produtoMarca.clear()
-    tela.produtoModelo.clear()
-    tela.produtoValor.clear()
-    tela.radioPeriferico.setChecked(False)
-    tela.radioHardware.setChecked(False)
-    tela.radioEletronico.setChecked(False)
+        # Abrir diálogo para seleção de arquivo
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getSaveFileName(tela_banco, "Salvar PDF", "", "PDF Files (*.pdf);;All Files (*)", options=options)
+        if fileName:
+        
+            # Configuração inicial do PDF
+            pdf = canvas.Canvas(fileName, pagesize=A4)
+            largura, altura = A4
+            pdf.setFont("Times-Bold", 25)
+            pdf.drawString(185, 800, "Produtos cadastrados")
+            
+            # Definição do cabeçalho da tabela
+            pdf.setFont("Times-Bold", 18)
+            pdf.drawString(20, 750, "CATEGORIA")
+            pdf.drawString(160, 750, "TIPO")
+            pdf.drawString(310, 750, "MARCA")
+            pdf.drawString(410, 750, "MODELO")
+            pdf.drawString(510, 750, "VALOR")
+            
+            pdf.setFont("Times-Roman", 12)
+            y = 0
+            
+            # Adicionar os dados ao PDF
+            for i in range(len(dados_lidos)):
+                y += 20
+                if 750 - y < 50:  # Verificar se é necessário adicionar uma nova página
+                    pdf.showPage()
+                    pdf.setFont("Times-Bold", 18)
+                    pdf.drawString(20, 800, "CATEGORIA")
+                    pdf.drawString(160, 800, "TIPO")
+                    pdf.drawString(310, 800, "MARCA")
+                    pdf.drawString(410, 800, "MODELO")
+                    pdf.drawString(510, 800, "VALOR")
+                    pdf.setFont("Times-Roman", 12)
+                    y = 0
+                
+                pdf.drawString(20, 750 - y, str(dados_lidos[i][1]))
+                pdf.drawString(160, 750 - y, str(dados_lidos[i][2]))
+                pdf.drawString(310, 750 - y, str(dados_lidos[i][3]))
+                pdf.drawString(410, 750 - y, str(dados_lidos[i][4]))
+                pdf.drawString(510, 750 - y, f"{dados_lidos[i][5]:.2f}")  # Formatando o valor para duas casas decimais
+            
+            pdf.save()
+            print('PDF GERADO')
+            QMessageBox.about(tela_cadastro, '[SUCESSO]', 'PDF gerado com sucesso!')
+            
+    except mysql.connector.Error as err:
+        print(f'Erro ao conectar ao banco de dados: {err}')
+        QMessageBox.about(tela_cadastro, '[ERRO]', f'Erro ao gerar PDF: {err}')
+
+
+def limpar_campos(tela_cadastro):
+
+    tela_cadastro.produtoTipo.clear()
+    tela_cadastro.produtoMarca.clear()
+    tela_cadastro.produtoModelo.clear()
+    tela_cadastro.produtoValor.clear()
+    tela_cadastro.radioPeriferico.setAutoExclusive(False)
+    tela_cadastro.radioHardware.setAutoExclusive(False)
+    tela_cadastro.radioEletronico.setAutoExclusive(False)
+    tela_cadastro.radioPeriferico.setChecked(False)
+    tela_cadastro.radioHardware.setChecked(False)
+    tela_cadastro.radioEletronico.setChecked(False)
+    tela_cadastro.radioPeriferico.setAutoExclusive(True)
+    tela_cadastro.radioHardware.setAutoExclusive(True)
+    tela_cadastro.radioEletronico.setAutoExclusive(True)
     
     
 
--- projeto_pyhton_facul/projeto.py
+++ projeto_pyhton_facul/projeto.py
@@ -1,6 +1,19 @@
 import funcoes
 from PyQt5 import uic,QtWidgets
 from PyQt5.QtWidgets import QMessageBox
+import mysql.connector
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+
+numero_id = 0
+
+banco = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "facul123",
+    database = "projeto_facul"
+
+)
 
 
 info_produtos = {}
@@ -9,48 +22,189 @@ produtos = []
 def cadastrar():
     print('funcao')
     
-    if(tela.radioPeriferico.isChecked()):
+    if(tela_cadastro.radioPeriferico.isChecked()):
         info_produtos['categoria'] = 'Periférico'
         
-    if(tela.radioHardware.isChecked()):
+    elif(tela_cadastro.radioHardware.isChecked()):
         info_produtos['categoria'] = 'Hardware'
         
-    if(tela.radioEletronico.isChecked()):
+    elif(tela_cadastro.radioEletronico.isChecked()):
         info_produtos['categoria'] = 'Eletrônico'
+
+    else:
+        QMessageBox.about(tela_cadastro, '[ALERTA]', 'CATEGORIA não escolhida')
+        return
+
+    
+    info_produtos['tipo'] = tela_cadastro.produtoTipo.text()
         
+    info_produtos['marca'] = tela_cadastro.produtoMarca.text()
+        
+    info_produtos['modelo'] = tela_cadastro.produtoModelo.text()
+
+    if not (info_produtos['tipo']):
+        QMessageBox.about(tela_cadastro, '[ALERTA]', 'TIPO não preenchido')
+        return
     
-    info_produtos['tipo'] = tela.produtoTipo.text()
+    elif not (info_produtos['marca']):
+         QMessageBox.about(tela_cadastro, '[ALERTA]', 'MARCA não preenchida')
+         return
     
-    info_produtos['marca'] = tela.produtoMarca.text()
-     
-    info_produtos['modelo'] = tela.produtoModelo.text()
-    
+    elif not (info_produtos['modelo']):
+         QMessageBox.about(tela_cadastro, '[ALERTA]', 'MODELO não preenchido')
+         return
+
+
     while True:
         try:
-            info_produtos['valor'] = float(tela.produtoValor.text())
-            tela.lblErro.setText('')
+            info_produtos['valor'] = float(tela_cadastro.produtoValor.text())
             
         except(ValueError, TypeError):
-            tela.lblErro.setText('[ERRO] Valor inválido. Por favor digite um número.')
-            return
+             QMessageBox.about(tela_cadastro, '[ALERTA]', 'Valor inválido')
+             return
         break
         
         
     produtos.append(info_produtos.copy())
-    info_produtos.clear()
-    funcoes.limpar_campos(tela)
+    
         
     print()
     for i in produtos:
         for k,v in i.items():
             print(f'{k}: {v}')
         print()
-              
-  
+
+    try:
+        cursor = banco.cursor()
+        comando_SQL = "INSERT INTO produtos (categoria, tipo, marca, modelo, valor) VALUES (%s, %s, %s, %s, %s)"
+        dados = (str(info_produtos['categoria']), str(info_produtos['tipo']), str(info_produtos['marca']), str(info_produtos['modelo']), str(info_produtos['valor']))
+        cursor.execute(comando_SQL, dados)
+        banco.commit()
+
+        QMessageBox.about(tela_cadastro, '[SUCESSO]', 'Produto cadastrado com sucesso!')
+
+        info_produtos.clear()
+        funcoes.limpar_campos(tela_cadastro)
+
+        if (tela_banco.isVisible() == True):
+            banco_de_dados()
+        
+        
+    except mysql.connector.Error as err:
+        QMessageBox.about(tela_cadastro, '[ALERTA]', f'Erro ao conectar ao banco de dados: {err}')
+
+
+def banco_de_dados():
+    tela_banco.show()
+
+    cursor = banco.cursor()
+    comando_SQL = "select * from produtos"
+    cursor.execute(comando_SQL)
+    dados_lidos = cursor.fetchall()
+
+    tela_banco.tableWidget.setRowCount(len(dados_lidos))
+    tela_banco.tableWidget.setColumnCount(6)
+    
+    for l in range (0, len(dados_lidos)):
+        for c in range (6):
+            tela_banco.tableWidget.setItem(l, c, QtWidgets.QTableWidgetItem(str(dados_lidos[l][c])))
+
+
+def editar_dados():
+    global numero_id
+
+    print('editar')
+    tela_editar.show()
+
+    linha_selecionada = tela_banco.tableWidget.currentRow()
+    
+
+    cursor = banco.cursor()
+    cursor.execute("SELECT id FROM produtos")
+    dados_lidos = cursor.fetchall()
+    valor_id = dados_lidos[linha_selecionada][0]
+    numero_id = valor_id
+    
+
+    cursor.execute("SELECT * FROM produtos WHERE id="+str(valor_id))
+    produto = cursor.fetchall()
+    print(produto)
+    
+    
+    tela_editar.editor_tipo.setText(str(produto[0][2]))
+    tela_editar.editor_marca.setText(str(produto[0][3]))
+    tela_editar.editor_modelo.setText(str(produto[0][4]))
+    tela_editar.editor_valor.setText(str(produto[0][5]))
+
+
+def salvar_dados():
+    global numero_id
+    print(numero_id)
+
+    if(tela_editar.radioED_periferico.isChecked()):
+        categoria = 'Periférico'
+        
+    elif(tela_editar.radioED_hardware.isChecked()):
+        categoria = 'Hardware'
+
+    elif(tela_editar.radioED_eletronico.isChecked()):
+        categoria = 'Eletrônico'
+
+    else:
+        QMessageBox.about(tela_editar, '[ALERTA]', 'CATEGORIA não escolhida')
+        return
+    
+    tipo = tela_editar.editor_tipo.text()
+    marca = tela_editar.editor_marca.text()
+    modelo = tela_editar.editor_modelo.text()
+    
+    if not (tipo):
+        QMessageBox.about(tela_editar, '[ALERTA]', 'TIPO não preenchido')
+        return
+    elif not (marca):
+        QMessageBox.about(tela_editar, '[ALERTA]', 'MARCA não preenchido')
+        return
+    elif not (modelo):
+        QMessageBox.about(tela_editar, '[ALERTA]', 'MODELO não preenchido')
+        return
+    
+    while True:
+        try:
+            valor = float(tela_editar.editor_valor.text())
+            
+            
+        except(ValueError, TypeError):
+             QMessageBox.about(tela_editar, '[ALERTA]', 'Valor inválido')
+             return
+        break
+
+    try:
+        QMessageBox.about(tela_editar, '[SUCESSO]', 'Dados Atualizados')
+        cursor = banco.cursor()
+        comando_sql = "UPDATE produtos SET categoria = %s, tipo = %s, marca = %s, modelo = %s, valor = %s WHERE id = %s"
+        valores = (categoria, tipo, marca, modelo, valor, numero_id)
+        cursor.execute(comando_sql, valores)
+        banco.commit()
+        tela_editar.close()
+        tela_banco.close()
+        banco_de_dados()
+        
+
+    except mysql.connector.Error as err:
+        QMessageBox.about(tela_editar, '[ALERTA]', f'Erro ao conectar ao banco de dados: {err}')
+
+
 app = QtWidgets.QApplication([])
-tela=uic.loadUi('tela_cadastro.ui')
+tela_cadastro=uic.loadUi('telas/tela_cadastro.ui')
+tela_banco=uic.loadUi('telas/tela_banco.ui')
+tela_editar=uic.loadUi('telas/tela_editar.ui')
 
-tela.btnCadastrar.clicked.connect(cadastrar)
+tela_cadastro.btnCadastrar.clicked.connect(cadastrar)
+tela_cadastro.btnBanco.clicked.connect(banco_de_dados)
+tela_banco.btnPDF.clicked.connect(lambda: funcoes.gerar_pdf(tela_cadastro, banco, tela_banco))
+tela_banco.btnExcluir.clicked.connect(lambda: funcoes.excluir_dados(tela_banco, banco))
+tela_banco.btnEditar.clicked.connect(editar_dados)
+tela_editar.btnSalvar.clicked.connect(salvar_dados)
 
-tela.show()
+tela_cadastro.show()
 app.exec()
